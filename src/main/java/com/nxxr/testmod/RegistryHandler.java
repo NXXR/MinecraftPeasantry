33b3b1ac37602a919ae1109e568352f993d6cda1
Mo Zeumer <m_zeumer@gmx.de>
2020-08-02 19:45:09 +0200

    Initial commit

---
+++ src/main/java/com/nxxr/testmod/RegistryHandler.java
@@ -0,0 +1,24 @@
+package com.nxxr.testmod;
+
+
+import com.nxxr.testmod.TestMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MODID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static final RegistryObject<Item> COPPER = ITEMS.register("copper", () ->
+            new Item(
+                    new Item.Properties().group(ItemGroup.MATERIALS)
+            )
+    );
+}
